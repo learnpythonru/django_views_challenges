df555419169856bd90267c48b04a5b35a2482770
Octopulus96 <octopulus96@gmail.com>
2024-02-23 08:51:33 +0300

    Решил задание из b_bye_user

--- django_views_routing_homework/urls.py
+++ django_views_routing_homework/urls.py
@@ -9,12 +9,14 @@ from django_views_routing_homework.views.level_2.d_authorization import authoriz
 from django_views_routing_homework.views.level_3.b_validate_user_data import validate_user_data_view
 from django_views_routing_homework.views.level_3.c_github_full_name import fetch_name_from_github_view
 from django_views_routing_homework.views.level_3.d_file_generation import generate_file_with_text_view
+from django_views_routing_homework.views.level_1.b_bye_user import bye_user_view
 
 from django_views_routing_homework.views.level_3.a_user_ip import show_user_ip_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('welcome/', welcome_user_view),
+    path('bye/', bye_user_view),
     path('banned/<slug:username>/', is_username_banned_view),
     path('user-info-by-username/<int:username>/', get_user_info_by_username_view),
     path('products/', get_products_view),
